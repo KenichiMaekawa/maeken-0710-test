689b9932b6c0d91fdb7c19ce505c31cb203759e0
KenichiMaekawa <maekawakenichi@maeken.local>
2015-07-10 16:56:08 +0900

    2nd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+  root to: 'home#index'
+
   resources :hoges
   #root :to => 'hoges#index'
   # The priority is based upon order of creation: first created -> highest priority.
